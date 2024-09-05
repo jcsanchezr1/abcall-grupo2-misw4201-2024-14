bc632200a593c6230b05c665c22106368597373e
ianbeltran427 <ip.beltran@uniandes.edu.co>
2024-09-04 23:01:52 -0500

    agrega primera versión del receptor, esta versión expone un servicio para guardar una llamada o obtener todas las llamdas

---
+++ Experimento1/Receptor/Readme.md
@@ -0,0 +1,13 @@
+#TODO: CREATE README WITH BELOW INFORMATION
+
+python3 -m venv venv\n
+source  venv/bin/activate
+pip install flask
+deactivate
+source  venv/bin/activate
+cd flaskr/
+pip install flask-cors
+pip install flask_sqlalchemy\n
+pip install marshmallow\n
+pip install marshmallow-sqlalchemy\n
+pip install flask_restful
---
+++ Experimento1/Receptor/flaskr/modelos/modelos.py
@@ -0,0 +1,39 @@
+import datetime
+import enum
+
+from flask_sqlalchemy import SQLAlchemy
+from marshmallow import fields, Schema
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+
+db = SQLAlchemy()
+
+class TipoApp(enum.Enum):
+    ACTIVO = "ACTIVO"
+    REDUNDANTE = "REDUNDANTE"
+
+class Auditoria(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    id_llamada = db.Column(db.String(50))
+    fecha_registro = db.Column(db.DateTime, default=datetime.datetime.now)
+    fecha_finalizacion = db.Column(db.DateTime, default=datetime.datetime.now)
+    tipo_app = db.Column(db.Enum(TipoApp))
+
+    def __repr__(self):
+        return "{}-{}-{}-{}-{}".format(self.id,self.id_llamada,self.fecha_registro,self.fecha_finalizacion,self.tipo_app)
+
+
+class EnumADiccionario(fields.Field):
+    def _serialize(self, value, attr, obj, **kwargs):
+        if value is None:
+            return None
+        return value.value
+
+class AuditoriaSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Auditoria
+        include_relationships = True
+        load_instance = True
+
+    tipo_app = EnumADiccionario(attribute='tipo_app')
+
+
