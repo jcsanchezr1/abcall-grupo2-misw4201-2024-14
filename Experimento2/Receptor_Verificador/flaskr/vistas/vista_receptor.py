fcaf30479ab2de89895c784407e363a333f3fcd6
jcsanchezr1 <jc.sanchezr1@uniandes.edu.co>
2024-09-27 19:02:53 -0500

    Refactor Vistas Receptor Verficador

---
+++ Experimento2/Receptor_Verificador/flaskr/vistas/vista_receptor.py
@@ -0,0 +1,49 @@
+import datetime
+import hashlib
+
+import requests
+from flask_restful import Resource, reqparse
+
+from ..modelos import db, Auditoria, TipoEstadoEstadoAuditoria
+
+
+class VistaReceptor(Resource):
+
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('payload', type=str, required=True, help="El payload es obligatorio")
+        args = parser.parse_args()
+
+        payload = args['payload']
+        checksum = hashlib.sha256(payload.encode('utf-8')).hexdigest()
+
+        nueva_auditoria = Auditoria()
+
+        nueva_auditoria.fecha_registro = datetime.datetime.now()
+        nueva_auditoria.checksum1 = checksum
+        nueva_auditoria.payload = payload
+        nueva_auditoria.estado_auditoria = TipoEstadoEstadoAuditoria.Registered
+
+        db.session.add(nueva_auditoria)
+        db.session.commit()
+
+        auditoria_id = nueva_auditoria.id
+
+        auditoria_enviada = {
+            "id": auditoria_id,
+            "payload": payload
+        }
+
+        gestor_incidentes_url = "http://gestor-incidentes/api/incidentes"
+
+        try:
+            respuesta = requests.post(gestor_incidentes_url, json=auditoria_enviada)
+
+            if respuesta.status_code == 200 or respuesta.status_code == 201:
+                return {"message": "Auditoría registrada y enviada correctamente"}, 201
+            else:
+                return {"message": "Error al enviar auditoría a GestorIncidentes'",
+                        "status_code": respuesta.status_code}, respuesta.status_code
+
+        except requests.exceptions.RequestException as e:
+            return {"message": "Fallo al conectar con GestorIncidentes", "error": str(e)}, 500
--- Experimento2/Receptor_Verificador/flaskr/vistas/vista_verificador.py
+++ Experimento2/Receptor_Verificador/flaskr/vistas/vista_verificador.py
@@ -1,14 +1,14 @@
-from flask_restful import Resource, reqparse
+import datetime
+import hashlib
+import json
+import threading
 
-from ..modelos import db, Auditoria, AuditoriaSchema,EstadoVerificacion, TipoEstadoEstadoAuditoria
-from flask import request, Flask, request, jsonify
+import requests
+from flask import request
+from flask_restful import Resource
 from google.cloud import pubsub_v1
 
-import datetime
-import json
-import requests
-import threading
-import hashlib
+from ..modelos import db, Auditoria, AuditoriaSchema, EstadoVerificacion, TipoEstadoEstadoAuditoria
 
 auditoria_schema = AuditoriaSchema()
 
@@ -24,7 +24,7 @@ subscription_path = subscriber_client.subscription_path(project_id, subscription
 
 class VistaAuditoria(Resource):
 
-   def put(self):
+    def put(self):
         try:
             id = request.json["id"]
             payload = request.json["payload"]
@@ -34,14 +34,12 @@ class VistaAuditoria(Resource):
 
             if not id or not payload:
                 print("Faltan datos en el mensaje.")
-                message.nack()
                 return
 
             auditoria = Auditoria.query.get(id)
             print(auditoria)
             if not auditoria:
                 print(f"Auditoría con id {id} no encontrada.")
-                message.nack()
                 return
 
             if auditoria.checksum1 == checksum:
@@ -62,46 +60,7 @@ class VistaAuditoria(Resource):
             print(e)
             return {'error': f'Ocurrió un error: {str(e)}'}, 500
 
-class VistaReceptor(Resource):
-    
-    def post(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('payload', type=str, required=True, help="El payload es obligatorio")
-        args = parser.parse_args()
-    
-        payload = args['payload']
-        checksum = hashlib.sha256(payload.encode('utf-8')).hexdigest()
 
-        nueva_auditoria = Auditoria()
-
-        nueva_auditoria.fecha_registro = datetime.datetime.now()
-        nueva_auditoria.checksum1 = checksum
-        nueva_auditoria.payload = payload
-        nueva_auditoria.estado_auditoria = TipoEstadoEstadoAuditoria.Registered
-
-        db.session.add(nueva_auditoria)
-        db.session.commit()
-
-        auditoria_id = nueva_auditoria.id
-
-        auditoria_enviada = {
-            "id": auditoria_id,
-            "payload": payload
-        }
-
-        gestor_incidentes_url = "http://gestor-incidentes/api/incidentes"
-
-        try: 
-            respuesta = requests.post(gestor_incidentes_url, json=auditoria_enviada)
-
-            if respuesta.status_code == 200 or respuesta.status_code == 201:
-                return {"message": "Auditoría registrada y enviada correctamente"}, 201
-            else:
-                return {"message": "Error al enviar auditoría a GestorIncidentes'",
-                        "status_code": respuesta.status_code}, respuesta.status_code
-
-        except requests.exceptions.RequestException as e:
-            return {"message": "Fallo al conectar con GestorIncidentes", "error": str(e)}, 500
 # Clase separada para manejar la suscripción y la escucha continua de mensajes en segundo plano
 class PubSubSubscriber:
     def __init__(self):
@@ -147,6 +106,7 @@ class PubSubSubscriber:
             future.result()
         except Exception as e:
             print(f"Listening interrupted: {e}")
+
 
 # Instancia del suscriptor (esto inicia la escucha de mensajes en segundo plano)
 subscriber = PubSubSubscriber()
